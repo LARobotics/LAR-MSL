8f9db00a0d110f0e9f6dd8e72f1705f2d9a6a630
RuiMartins1024 <135147947+RuiMartins1024@users.noreply.github.com>
2025-02-26 20:29:46 +0000

    Delete Control directory

--- Control/IPCom.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.append(path + '/InterProcessCommunication/')
-import InterProcessCommunication as IPC  # noqa: E402
-
-Handler = IPC.Comms("Ctrl", "CDB")
